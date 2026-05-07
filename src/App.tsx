cee09749e98b0c9d9f41b0b56f8f05520c12c056
Leandro Henrique <arqleandro.hensilva@gmail.com>
2026-05-07 20:21:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login.tsx";
 import Pipeline from "./pages/Pipeline.tsx";
 import PropostaArqint from "./pages/PropostaArqint.tsx";
 import PropostaInt from "./pages/PropostaInt.tsx";
+import PropostaComercial from "./pages/PropostaComercial.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -24,6 +25,7 @@ const App = () => (
           <Route path="/pipeline" element={<Pipeline />} />
           <Route path="/proposta/arqint" element={<PropostaArqint />} />
           <Route path="/proposta/int" element={<PropostaInt />} />
+          <Route path="/proposta/comercial" element={<PropostaComercial />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
